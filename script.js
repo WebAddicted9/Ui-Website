5cdeb7b018e814cccc00c6efb61e6d45f978034a
Rahulvaishnav9 <155007617+Rahulvaishnav9@users.noreply.github.com>
2023-12-27 12:12:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+ <script>
+        const darkModeToggle = document.getElementById('darkModeToggle');
+        const body = document.body;
+        const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+        if (isDarkMode){
+            body.classList.add('dark-mode');
+            darkModeToggle.checked = true;
+        }
+        darkModeToggle.addEventListener('change', () => {
+            if (darkModeToggle.checked){
+                body.classList.add('dark-mode');
+                localStorage.setItem('darkMode', 'enabled');
+            }
+            else{
+                body.classList.remove('dark-mode');
+                localStorage.setItem('darkMode', 'disabled')
+            }
+        });
+    </script>
